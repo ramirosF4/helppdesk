e19f9da6f7f96e67c7ae14ff4734e46e55e6b1b2
ramirosF4 <ramirohv23@gmail.com>
2026-03-14 16:01:54 -0600

    Visualizacion de datos en modal, modificacion de usuarios js, cap13

--- public/js/usuarios/usuarios.js
+++ public/js/usuarios/usuarios.js
@@ -30,7 +30,17 @@ function obtenerDatosUsuario(idUsuario){
         url: "../procesos/usuarios/crud/obtenerDatosUsuario.php",
         success: function(respuesta){
             respuesta = JSON.parse(respuesta);
-            console.log(respuesta);
+            $('#idUsuario').val(respuesta['idUsuario']); 
+            $('#paternou').val(respuesta['paterno']); 
+            $('#maternou').val(respuesta['materno']); 
+            $('#nombreu').val(respuesta['nombrePersona']); 
+            $('#fechaNacimientou').val(respuesta['fechaNacimiento']); 
+            $('#sexou ').val(respuesta['sexo']); 
+            $('#telefonou').val(respuesta['telefono']); 
+            $('#correou').val(respuesta['correo']); 
+            $('#usuariou').val(respuesta['nombreUsuario']); 
+            $('#idRolu').val(respuesta['idRol']); 
+            $('#ubicacionu').val(respuesta['ubicacion']); 
         }
     });
 
--- views/usuarios/modalActualizar.php
+++ views/usuarios/modalActualizar.php
@@ -9,6 +9,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+        <input type="text" id="idUsuario" name="idUsuario" hidden>
           <div class="row">
               <div class="col-sm-4">
                     <label for="paternou">Apellido paterno</label>
