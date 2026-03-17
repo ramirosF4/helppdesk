6a373a87676ac5a372241c618a589d0f1aa27d82
ramirosF4 <ramirohv23@gmail.com>
2026-03-16 22:47:28 -0600

    Presentacion de datos al inicoo de pantalla de carga, cap 30

--- views/inicio.php
+++ views/inicio.php
@@ -1,6 +1,7 @@
 <?php 
     include "header.php"; 
     if(isset($_SESSION['usuario']) && ($_SESSION['usuario']['rol'] == 1 || $_SESSION['usuario']['rol'] == 2)){
+        $idUsuario = $_SESSION['usuario']['id'];
 ?>
 
 
@@ -8,14 +9,33 @@
     <div class="container">
         <div class="card border-0 shadow my-5">
             <div class="card-body p-5">
-            <h1 class="fw-light">inicio</h1>
-            <p class="lead">Content on the page will</p>  
+                <h1 class="fw-light">Bienvenido <?php echo $_SESSION['usuario']['nombre'];?></h1>
+                <p class="lead">
+                    <div class="row">
+                        <div class="col-sm-4">Apellido paterno:  <span id="paterno"></span> </div>
+                        <div class="col-sm-4">Apellido materno: <span id="materno"></span> </div>
+                        <div class="col-sm-4">Nombre: <span id="nombre"></span> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">Telefono: <span id="telefono"></span> </div>
+                        <div class="col-sm-4">Correo: <span id="correo"></span> </div>
+                        <div class="col-sm-4">Edad: <span id="edad"></span> </div>
+                    </div>
+                </p>  
             </div>
         </div>
     </div>
 
     <?php 
         include "footer.php"; 
+    ?>
+    <script src="../public/js/inicio/personales.js"></script>
+
+    <script>
+        let idUsuario = '<?php echo $idUsuario; ?>'
+        datosPersonalesInicio(idUsuario)
+    </script>
+    <?php
     }else{
         header("location:../index.html"); 
     }
